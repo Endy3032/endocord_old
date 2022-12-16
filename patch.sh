910c4a2e6009e11d7b20c253a8fc0d61e99b3003
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-12-16 20:37:08 +0700

    remove anime, icon packs, flowercord and more
    Former-commit-id: 41e517b68e7edc4f4c75a3e8ecd2c7aa24082f44

--- patch.sh
+++ patch.sh
@@ -25,8 +25,8 @@ wait $!
 MAIN_PLIST=Payload/Discord.app/Info.plist
 
 # patch discord's name
-plutil -replace CFBundleName -string "Rosiecord" $MAIN_PLIST
-plutil -replace CFBundleDisplayName -string "Rosiecord" $MAIN_PLIST
+plutil -replace CFBundleName -string "Enmity" $MAIN_PLIST
+plutil -replace CFBundleDisplayName -string "Enmity" $MAIN_PLIST
 
 # patch discord's url scheme to add enmity's url handler
 plutil -insert CFBundleURLTypes.1 -xml "<dict><key>CFBundleURLName</key><string>Enmity</string><key>CFBundleURLSchemes</key><array><string>enmity</string></array></dict>" $MAIN_PLIST
@@ -34,37 +34,14 @@ plutil -insert CFBundleURLTypes.1 -xml "<dict><key>CFBundleURLName</key><string>
 # remove discord's device limits
 plutil -remove UISupportedDevices $MAIN_PLIST
 
-# patch the icons
-cp -rf Icons/* Payload/Discord.app/ 
-plutil -replace CFBundleIcons -xml "<dict><key>CFBundlePrimaryIcon</key><dict><key>CFBundleIconFiles</key><array><string>EnmityIcon60x60</string></array><key>CFBundleIconName</key><string>EnmityIcon</string></dict></dict>" $MAIN_PLIST
-plutil -replace CFBundleIcons~ipad -xml "<dict><key>CFBundlePrimaryIcon</key><dict><key>CFBundleIconFiles</key><array><string>EnmityIcon60x60</string><string>EnmityIcon76x76</string></array><key>CFBundleIconName</key><string>EnmityIcon</string></dict></dict>" $MAIN_PLIST
-
 # patch itunes and files
 plutil -replace UISupportsDocumentBrowser -bool true $MAIN_PLIST
 plutil -replace UIFileSharingEnabled -bool true $MAIN_PLIST
 
-zip -r dist/Rosiecord_GGSans-Font.ipa Payload
+zip -r dist/GGSans-Font.ipa Payload
 
-# change the font
-cp -rf Fonts/woff2/* Payload/Discord.app/
-
-# pack the ipa into rosiecord and remove the payload and ipa
-zip -r dist/Rosiecord_Cabin-Font.ipa Payload
+# change the font and remove the payload and ipa
 rm -rf Payload
-
-# make the flowercord package
-
-# remove dir contents incase it exists
-rm -rf Flowercord_Patcher/packages/*
-
-# package the deb
-cd ./Flowercord_Patcher
-/usr/bin/make package
-
-# move it into Enmity_Patches
-PACKAGES=$(ls packages)
-PATCH_NAME=flowercord
-mv packages/$PACKAGES ../Enmity_Patches/Optional/$PATCH_NAME.deb
 
 # go back to main dir
 cd ..
@@ -75,33 +52,18 @@ cd ..
 wait $!
 
 # inject all of the patches into the enmity ipa
-for Patch in $(ls Enmity_Patches/Required) 
+for Patch in $(ls Enmity_Patches/Required)
 do
-    Azule/azule -i Dist/Rosiecord_Cabin-Font.ipa -o Dist -f Enmity_Patches/Required/${Patch} &
+    Azule/azule -i Dist/GGSans-Font.ipa -o Dist -f Enmity_Patches/Required/${Patch} &
     wait $!
-    mv Dist/Rosiecord_Cabin-Font+${Patch}.ipa Dist/Rosiecord_Cabin-Font.ipa
-
-    Azule/azule -i Dist/Rosiecord_GGSans-Font.ipa -o Dist -f Enmity_Patches/Required/${Patch} &
-    wait $!
-    mv Dist/Rosiecord_GGSans-Font+${Patch}.ipa Dist/Rosiecord_GGSans-Font.ipa
+    mv Dist/GGSans-Font+${Patch}.ipa Dist/GGSans-Font.ipa
 done
 
 # create a new ipa with each pack injected from the base ipa
 for Pack in $(ls Packs)
 do
-    unzip Dist/Rosiecord_Cabin-Font.ipa
+    unzip Dist/GGSans-Font.ipa
     cp -rf Packs/${Pack}/* Payload/Discord.app/assets/
-    zip -r Dist/Rosiecord_Cabin-Font+${Pack}_Icons.ipa Payload
+    zip -r Dist/GGSans-Font+${Pack}_Icons.ipa Payload
     rm -rf Payload
-
-    unzip Dist/Rosiecord_GGSans-Font.ipa
-    cp -rf Packs/${Pack}/* Payload/Discord.app/assets/
-    zip -r Dist/Rosiecord_GGSans-Font+${Pack}_Icons.ipa Payload
-    rm -rf Payload
-done
-
-for Ipa in $(ls Dist)
-do
-    Azule/azule -i Dist/$Ipa -o Dist -f Enmity_Patches/Optional/flowercord.deb &
-    wait $!
 done

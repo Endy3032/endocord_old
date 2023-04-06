e72961298488b8f0230d5765d3d255874d3e3b0e
Endy3032 <endy@endy3032.ml>
2023-04-06 21:39:30 +0700

    Update

--- Enmity/patch.sh
+++ Enmity/patch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Global variablse
-VERSION=164
+VERSION=174.0_42188
 IPA_NAME=Discord_$VERSION.ipa
-
-IPA_DIR=IPA/$IPA_NAME
-DISCORD_IDS=1011346757214543875/1069326339238273174
+DISCORD_IDS=1085420899907412028/1092882978117517373
+ENMITY_DEB=$(ls Debs | grep En)
+K2G_DEB=$(ls Debs | grep -i K2)
+SCROLL_DEB=$(ls Debs | grep -i scroll)
 
 PLIST=Payload/Discord.app/Info.plist
 
@@ -14,35 +15,33 @@ PLIST=Payload/Discord.app/Info.plist
 #-------------#
 
 # Wait for Discord IPA to download
-curl --create-dirs -O --output-dir IPA "https://cdn.discordapp.com/attachments/$DISCORD_IDS/$IPA_NAME" &
-wait $!
+# curl --create-dirs -O --output-dir IPA "https://cdn.discordapp.com/attachments/${DISCORD_IDS}/${IPA_NAME}"
 
 # Clean up
-mkdir -p Dist/
+mkdir -p Dist
 rm -rf Dist/*
 rm -rf Payload
 
 # Wait for IPA to unzip
-unzip -qq $IPA_DIR > /dev/null &
-wait $!
+unzip -qq IPA/${IPA_NAME}
 
 #--------------#
 # Modification #
 #--------------#
 
 # App name
-plutil -replace CFBundleName -string "Enmity" $PLIST
-plutil -replace CFBundleDisplayName -string "Enmity" $PLIST
+plutil -replace CFBundleName -string "Enmity" ${PLIST}
+plutil -replace CFBundleDisplayName -string "Enmity" ${PLIST}
 
 # Add Enmity URL scheme
 plutil -insert CFBundleURLTypes.1 -xml "<dict><key>CFBundleURLName</key><string>Enmity</string><key>CFBundleURLSchemes</key><array><string>enmity</string></array></dict>" $PLIST
 
 # Remove device limits
-plutil -remove UISupportedDevices $PLIST
+plutil -remove UISupportedDevices ${PLIST}
 
 # Enable iTunes file sharing
-plutil -replace UISupportsDocumentBrowser -bool true $PLIST
-plutil -replace UIFileSharingEnabled -bool true $PLIST
+plutil -replace UISupportsDocumentBrowser -bool true ${PLIST}
+plutil -replace UIFileSharingEnabled -bool true ${PLIST}
 
 # Replace Icons
 cp -rf Icons/* Payload/Discord.app/assets/
@@ -56,13 +55,11 @@ rm -rf Payload
 #-------#
 
 # Get Azule
-[[ -d "Azule" ]] && echo "[*] Azule already exists" || git clone https://github.com/Al4ise/Azule &
-wait $!
+[[ -d "Azule" ]] && echo "[*] Azule already exists" || git clone https://github.com/Al4ise/Azule
 
 # Inject tweaks
-for Patch in $(ls Debs)
-do
-    Azule/azule -i Dist/Enmity_v${VERSION}.ipa -f Debs/${Patch} -o Dist &
-    wait $!
-    mv Dist/Enmity_v${VERSION}+${Patch}.ipa Dist/Enmity_v${VERSION}.ipa
-done
+Azule/azule -i "Dist/Enmity_v${VERSION}.ipa" -f "${PWD}/Debs/${ENMITY_DEB}" "${PWD}/Debs/${SCROLL_DEB}" -e -o "Dist"
+mv "Dist/Enmity_v${VERSION}+${ENMITY_DEB}+${SCROLL_DEB}.ipa" "Dist/Enmity_v${VERSION}.ipa"
+
+Azule/azule -i "Dist/Enmity_v${VERSION}.ipa" -f "${PWD}/Debs/${K2G_DEB}" -e -o "Dist"
+mv "Dist/Enmity_v${VERSION}+${K2G_DEB}.ipa" "Dist/Enmity_v${VERSION}_K2G.ipa"

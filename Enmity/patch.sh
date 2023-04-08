5624c0febd8fe757a68f21ebd1480df87b09dab5
Endy3032 <endy@endy3032.ml>
2023-04-08 12:49:48 +0700

    updated script

--- Enmity/patch.sh
+++ Enmity/patch.sh
@@ -1,34 +1,52 @@
 #!/bin/zsh
 
-# Global variablse
 chdir Enmity
+
+# Global variablse
 VERSION=174.0_42188
-IPA_NAME=Discord_$VERSION.ipa
+BASE_IPA_NAME=Discord_$VERSION.ipa
 DISCORD_IDS=1085420899907412028/1092882978117517373
+IPA_LINK=https://cdn.discordapp.com/attachments/${DISCORD_IDS}/${BASE_IPA_NAME}
+
+ENMITY_IPA_NAME=Enmity_v${VERSION}
+K2GENMITY_IPA_NAME=K2GEnmity_v${VERSION}
+
 ENMITY_DEB=$(ls Debs | grep En)
 K2G_DEB=$(ls Debs | grep K2)
 SCROLL_DEB=$(ls Debs | grep scroll)
 
 PLIST=Payload/Discord.app/Info.plist
 
+function print() {
+	echo "\x1b[38;2;136;192;208m[*] $1\x1b[0m"
+}
+
 #-------------#
 # Preparation #
 #-------------#
 
 # Wait for Discord IPA to download
-curl --create-dirs -O --output-dir IPA "https://cdn.discordapp.com/attachments/${DISCORD_IDS}/${IPA_NAME}"
+if [[ -e "IPA/${BASE_IPA_NAME}" ]] then print "IPA already exists"
+else
+	print "Fetching IPA"
+	curl -O --output-dir IPA --create-dirs ${IPA_LINK}
+fi
 
 # Clean up
+print "Cleaning up"
+rm -rf Dist
 mkdir -p Dist
-rm -rf Dist/*
 rm -rf Payload
 
 # Wait for IPA to unzip
-unzip -qq IPA/${IPA_NAME}
+print "Unzipping..."
+unzip -qq IPA/${BASE_IPA_NAME} & wait $!
 
 #--------------#
 # Modification #
 #--------------#
+
+print "Modifying IPA"
 
 # App name
 plutil -replace CFBundleName -string "Enmity" ${PLIST}
@@ -48,7 +66,13 @@ plutil -replace UIFileSharingEnabled -bool true ${PLIST}
 cp -rf Icons/* Payload/Discord.app/assets/
 
 # Package
-zip -r dist/Enmity_v${VERSION}.ipa Payload
+print "Packaging Enmity..."
+zip -qr Dist/${ENMITY_IPA_NAME}.ipa Payload
+
+print "Packaging K2GEnmity..."
+plutil -replace NSFaceIDUsageDescription -string "K2genmity" ${PLIST}
+zip -qr Dist/${K2GENMITY_IPA_NAME}.ipa Payload
+
 rm -rf Payload
 
 #-------#
@@ -56,11 +80,21 @@ rm -rf Payload
 #-------#
 
 # Get Azule
-[[ -d "Azule" ]] && echo "[*] Azule already exists" || git clone https://github.com/Al4ise/Azule
+if [[ -d "Azule" ]] then print "Azule already exists"
+else
+	print "Fetching Azule"
+	git clone "https://github.com/Al4ise/Azule.git"
+fi
 
 # Inject tweaks
-Azule/azule -i "Dist/Enmity_v${VERSION}.ipa" -f "${PWD}/Debs/${ENMITY_DEB}" "${PWD}/Debs/${SCROLL_DEB}" -o "Dist"
-mv "Dist/Enmity_v${VERSION}+${ENMITY_DEB}+${SCROLL_DEB}.ipa" "Dist/Enmity_v${VERSION}.ipa"
+for IPA in Dist/*.ipa
+do
+	print "Injecting tweaks into ${IPA}"
+	Azule/azule -i ${IPA} -f ${PWD}/Debs/${ENMITY_DEB} ${PWD}/Debs/${SCROLL_DEB} -o Dist
+	mv $(echo ${IPA} | sed s/\.ipa//)+${ENMITY_DEB}+${SCROLL_DEB}.ipa ${IPA}
+done
 
-Azule/azule -i "Dist/Enmity_v${VERSION}.ipa" -f "${PWD}/Debs/${K2G_DEB}" -o "Dist"
-mv "Dist/Enmity_v${VERSION}+${K2G_DEB}.ipa" "Dist/Enmity_v${VERSION}_K2G.ipa"
+# Inject K2G
+print "Injecting K2G"
+Azule/azule -i Dist/${K2GENMITY_IPA_NAME}.ipa -f ${PWD}/Debs/${K2G_DEB} -o Dist
+mv Dist/${K2GENMITY_IPA_NAME}+${K2G_DEB}.ipa Dist/${K2GENMITY_IPA_NAME}.ipa
